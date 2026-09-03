749d0717d02325f59bde441fb9887a37c6709ae8
Adithya9390 <kolliadithya29@gmail.com>
2026-09-03 21:52:07 +0530

    ite.config1.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,3 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   base: "/dharmalens/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
